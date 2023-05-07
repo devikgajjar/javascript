4ffcf70ea38e3dd6dc119e3f3724197ec19302c2
devikgajjar <132844242+devikgajjar@users.noreply.github.com>
2023-05-07 19:11:28 +0000

    comit done

---
+++ basic/test.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"\
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+
+
+console.log(accountId);
+
